756c70e956d068531e4cee196e5e52cd5dcb6af2
Blake <108043930+CyberNerdBlake@users.noreply.github.com>
2023-12-30 00:22:58 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+from colorama import Fore
+
+mins = int(input("Enter time of mins: "))
+Seconds = int(input("Enter time of Seconds: "))
+
+Seconds = mins * 60 + Seconds
+for x in range(Seconds):
+    SecondsRemaining = Seconds - x
+    Secondhand = SecondsRemaining % 60
+    Minhand = int( SecondsRemaining / 60) % 60
+    Hourhand = int(SecondsRemaining / 3600)
+    #print(f"Seconds remaining: {Fore.GREEN}{Seconds - x}{Fore.RESET}")
+    print(f"Timer:{Fore.GREEN}{Hourhand:02}:{Minhand:02}:{Secondhand:02}{Fore.RESET}",end="\r")
+    time.sleep(1)
