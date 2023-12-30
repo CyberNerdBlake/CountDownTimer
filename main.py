3062e37e4725ab976356241f8b9c57e0e9b3b7f1
Blake <108043930+CyberNerdBlake@users.noreply.github.com>
2023-12-30 00:51:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import time
 from colorama import Fore
 
+
+LOGO ="""
+        _____
+     _.'_____`._
+   .'.-'  12 `-.`.
+  /,' 11      1 `.\\
+ // 10      /   2 \\\\
+;;         /       ::
+|| 9  ----O      3 ||
+::                 ;;
+ \\\\ 8           4 //
+  \`. 7       5 ,'/
+   '.`-.__6__.-'.'
+    ((-._____.-))
+    _))       ((_
+   '--'       '--'"""
+print(LOGO)
 mins = int(input("Enter time of mins: "))
 Seconds = int(input("Enter time of Seconds: "))
 
